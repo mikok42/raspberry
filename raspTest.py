82fe92f2ee77f4eed666f1863e663fdf6be7138a
Mikołaj Linczewski <74317275+mikok42@users.noreply.github.com>
2021-06-07 12:24:35 +0200

    Update raspTest.py

--- raspTest.py
+++ raspTest.py
@@ -1,13 +1,11 @@
 from gpiozero import LED
 from time import sleep
 
-ledGreen = LED(8)
-ledOrange = LED(18)
+ledGreen = LED(17)
+
 
 while True:
     ledGreen.on()
     sleep(1)
     ledGreen.off()
-    ledOrange.on()
-    sleep(1)
-    ledOrange.off()
+   
